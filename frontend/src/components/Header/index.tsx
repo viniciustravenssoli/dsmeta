d9c06db69ec209e29ede3be56b44782a18110d71
vts1978 <vinicioussouza@gmail.com>
2022-07-11 18:17:01 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                     Vinicius Travenssoli durante o evento Spring React Dev Superior
+                </p>
+            </div>
+        </header>
+
+
+    )
+}
+
+export default Header
