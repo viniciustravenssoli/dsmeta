35118e8ee03812bb825d20d28128a7cbe5f22011
vts1978 <vinicioussouza@gmail.com>
2022-07-11 17:46:20 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Ola mundo</h1>
+  return (
+    <>
+      <h1>Ola mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
