1cc25f0930d2ec6bd63aecbc7645fc4d9ff7a7f4
vts1978 <vinicioussouza@gmail.com>
2022-07-15 19:09:05 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notificantion-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
@@ -12,7 +13,7 @@ type Props = {
 
 function handleClick(id : number) {
     axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
-        console.log("Sucesso");
+        toast.info("SMS enviado com sucesso");
     })
 }
 
